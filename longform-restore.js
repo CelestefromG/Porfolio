c294a90823f46e88b2c40fc8830d01e8829ec371
CelestefromG <siriusaddress336@gmail.com>
2026-08-14 07:12:48 +0800

    Load home scene polish styles

--- longform-restore.js
+++ longform-restore.js
@@ -1,1 +1,1 @@
-import('./about-language-fix.js?v=20260814-1');
+import('./about-language-fix.js?v=20260814-1');const l=document.createElement('link');l.rel='stylesheet';l.href='./home-scene-polish.css?v=20260814-1';document.head.appendChild(l);
