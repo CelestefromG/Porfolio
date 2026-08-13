a21e843bdaa3c0b77942359a2597a5d63d645b82
CelestefromG <siriusaddress336@gmail.com>
2026-08-14 07:16:53 +0800

    Load refined home typography override

--- longform-restore.js
+++ longform-restore.js
@@ -1,1 +1,1 @@
-import('./about-language-fix.js?v=20260814-1');const l=document.createElement('link');l.rel='stylesheet';l.href='./home-scene-polish.css?v=20260814-1';document.head.appendChild(l);
+import('./about-language-fix.js?v=20260814-1');for(const h of ['./home-scene-polish.css?v=20260814-1','./home-font-polish.css?v=20260814-1']){const l=document.createElement('link');l.rel='stylesheet';l.href=h;document.head.appendChild(l)}
